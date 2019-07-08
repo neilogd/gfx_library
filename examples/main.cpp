bff37c5638f209e00aec41e60dbe6bded9e10c27
Neil Forbes-Richardson <neilo@neilo.gd>
2019-07-08 23:09:53 +1000

    Test blinky working. Interrupt vector was incorrect.

--- examples/main.cpp
+++ examples/main.cpp
@@ -117,28 +117,23 @@ int main()
 }
 #else
 
+#include "stm32f1xx_hal.h"
+
 #include "isr.h"
 
-uint32_t Systick_Count = 0;
 uint32_t RTC_Count = 0;
 
 template<>
 struct IRQHandler<VectorTableEntry::Systick>
 {
-    static constexpr auto LambdaHandler = [](){ Systick_Count++; };
-    using constant_t = std::integral_constant<void(*)(), +LambdaHandler>;
-};
-
-template<>
-struct IRQHandler<VectorTableEntry::RTC>
-{
-    static constexpr auto LambdaHandler = [](){ RTC_Count++; };
+    static constexpr auto LambdaHandler = [](){ 
+        HAL_IncTick();
+        HAL_SYSTICK_IRQHandler();
+    };
     using constant_t = std::integral_constant<void(*)(), +LambdaHandler>;
 };
 
 #include "vectors.h"
-
-#include "stm32f1xx_hal.h"
 
 class Dummy_Canvas : public Canvas
 {
@@ -175,9 +170,61 @@ public:
     }
 };
 
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = 16;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
+
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+    /* SysTick_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+void GPIO_Config()
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+    
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    
+    /* Set GPIOD Pin#15 Parameters */
+    GPIO_InitStruct.Pin = GPIO_PIN_13;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
 
 int main()
 {
+    HAL_Init();
+    SystemClock_Config();
+    GPIO_Config();
+    
+    for (;;)
+    {
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_Delay(100);
+    }
+
+#if 0
     HAL_Init();
 
     ExampleInit();
@@ -187,8 +234,10 @@ int main()
     while(true)
     {
         ExampleTick(canvas);
-    }
 
+        HAL_Delay(33); // ~30fps tick, ish.
+    }
+#endif
     return 0;
 
 }
